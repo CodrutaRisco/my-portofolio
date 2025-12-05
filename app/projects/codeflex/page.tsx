de5d3d15dda410f94650619368d83032ffaf41b9
CodrutaRisco <codruta.risco@gmail.com>
2025-12-05 16:10:17 +0000

    Fix: remove unused Image import

--- app/projects/codeflex/page.tsx
+++ app/projects/codeflex/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image"; // Uncomment when adding screenshots
 
 export default function CodeFlexProjectPage() {
   return (
